2e6ed111c7220550139667207886f2432f96d925
cmgvc <chloe.gavv@gmail.com>
2024-12-05 16:10:47 -0500

    init home, nav, footer

--- frontend-flock/src/App.js
+++ frontend-flock/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
