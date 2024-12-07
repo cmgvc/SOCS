35c7a73b3a76a4a1bbfb78ef815f044a531690ab
Danielle Wahrhaftig <daniellewahrhaftig@gmail.com>
2024-12-07 15:42:06 -0500

    added icons for login page

--- frontend-flock/src/components/LoginForm.jsx
+++ frontend-flock/src/components/LoginForm.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { ReactComponent as UserIcon } from "../svg/user-1.svg";
+import { ReactComponent as MailIcon } from "../svg/mail.svg";
+import { ReactComponent as KeyIcon } from "../svg/key.svg";
+
 import "./login-form.css";
 
 const LoginForm = () => {
@@ -11,12 +15,15 @@ const LoginForm = () => {
     <div className="login-form-container">
       <form className="login-form" onSubmit={handleSubmit}>
         <div className="form-group">
+          <UserIcon className="input-icon" />
           <input type="text" placeholder="Name" required />
         </div>
         <div className="form-group">
+          <MailIcon className="input-icon" />
           <input type="email" placeholder="Email" required />
         </div>
         <div className="form-group">
+          <KeyIcon className="input-icon" />
           <input type="password" placeholder="Password" required />
         </div>
         <button type="submit" className="login-button">
@@ -25,7 +32,10 @@ const LoginForm = () => {
       </form>
 
       <p className="login-footer">
-        Already have an account? <a href="/auth">Log in here</a>
+        Already have an account?{" "}
+        <a href="/auth">
+          <strong>Log in here</strong>
+        </a>
       </p>
     </div>
   );
