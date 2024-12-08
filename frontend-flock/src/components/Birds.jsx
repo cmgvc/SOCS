d4a2a76a96ce06c19cff9135b0c307f3c3c1d15b
Danielle Wahrhaftig <daniellewahrhaftig@gmail.com>
2024-12-08 13:10:01 -0500

    added birds background

---
+++ frontend-flock/src/components/Birds.jsx
@@ -0,0 +1,7 @@
+import BirdsSvg from "../svg/birds.svg";
+
+const Birds = () => {
+  return <img src={BirdsSvg} alt="Birds SVG" fill="red" />;
+};
+
+export default Birds;
--- frontend-flock/src/pages/Login.js
+++ frontend-flock/src/pages/Login.js
@@ -1,12 +1,16 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import { ReactComponent as Birds } from "../svg/birds2.svg";
 import "./login.css";
 
 const Login = () => {
   return (
     <div className="login-page">
-      <h1 class="login-title">Create a new Flock Account</h1>
-      <LoginForm />
+      <Birds className="birds-background" />
+      <div className="login-content">
+        <h1 className="login-title">Create a new Flock Account</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 };
