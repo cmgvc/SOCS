920cfe274a7d40316388b330c39ee8dc8a13e243
Danielle Wahrhaftig <daniellewahrhaftig@gmail.com>
2024-12-11 21:06:50 -0500

    fixed arrow on button clicked

--- frontend-flock/src/components/CreateBooking/DropdownMenu.jsx
+++ frontend-flock/src/components/CreateBooking/DropdownMenu.jsx
@@ -38,6 +38,11 @@ const DropdownMenu = ({ options, defaultOption, onChange }) => {
     if (onChange) onChange(option); // Notify parent about the selection
   };
 
+  const handleButtonClicked = () => {
+    setIsOpen(!isOpen);
+    setIsSelected(false);
+  };
+
   return (
     <div className="dropdown-container-booking" ref={dropdownRef}>
       <button
@@ -45,7 +50,7 @@ const DropdownMenu = ({ options, defaultOption, onChange }) => {
           isSelected ? "dropdown-button-selected" : "dropdown-button-booking"
         }
         aria-expanded={isOpen}
-        onClick={() => setIsOpen(!isOpen)} // Toggle dropdown visibility
+        onClick={() => handleButtonClicked()} // Toggle dropdown visibility
       >
         {selectedOption} <span className="arrow">&#9662;</span>
       </button>
@@ -67,47 +72,3 @@ const DropdownMenu = ({ options, defaultOption, onChange }) => {
 };
 
 export default DropdownMenu;
-
-// import React, { useState, useEffect } from "react";
-// import PropTypes from "prop-types";
-
-// const DropdownMenu = ({ options, defaultOption, onChange }) => {
-//   const [selectedOption, setSelectedOption] = useState(defaultOption);
-
-//   // Sync dropdown state with the parent-provided defaultOption
-//   useEffect(() => {
-//     setSelectedOption(defaultOption);
-//   }, [defaultOption]);
-
-//   const handleOptionClick = (option) => {
-//     setSelectedOption(option);
-//     if (onChange) onChange(option);
-//   };
-
-//   return (
-//     <div className="dropdown-container">
-//       <button className="dropdown-button">
-//         {selectedOption} <span className="arrow">&#9662;</span>
-//       </button>
-//       <ul className="dropdown-menu">
-//         {options.map((option, index) => (
-//           <li
-//             key={index}
-//             onClick={() => handleOptionClick(option)}
-//             className="dropdown-item"
-//           >
-//             {option}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// DropdownMenu.propTypes = {
-//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-//   defaultOption: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
-
-// export default DropdownMenu;
